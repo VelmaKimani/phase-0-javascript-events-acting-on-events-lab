08886392aef2e3604baf46fac606d54c22da4d4d
Velma Kimani <stanvelma16@gmail.com>
2023-09-18 10:14:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -14,10 +14,16 @@ document.addEventListener("keydown", function (e) {
   });
 
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
   
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`;
+    if (right > 0) {
+      dodger.style.left = `${right + 1}px`;
     }
 }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
